7fa26e15e58a57fffcc929d6e69da2dd07701379
hitpossible <hitmouse1000@hotmail.com>
2023-03-15 22:02:28 +0700

    Add project files.

---
+++ VBCRUD/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtFirstname = New System.Windows.Forms.TextBox()
+        Me.txtLastname = New System.Windows.Forms.TextBox()
+        Me.cbGender = New System.Windows.Forms.ComboBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(142, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(356, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CREATE. READ, UPDATE AND DELETE WITH SEARCH"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Firstname"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(55, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Lastname"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(55, 221)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Gender"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(142, 83)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(230, 27)
+        Me.txtID.TabIndex = 5
+        '
+        'txtFirstname
+        '
+        Me.txtFirstname.Location = New System.Drawing.Point(142, 129)
+        Me.txtFirstname.Name = "txtFirstname"
+        Me.txtFirstname.Size = New System.Drawing.Size(230, 27)
+        Me.txtFirstname.TabIndex = 6
+        '
+        'txtLastname
+        '
+        Me.txtLastname.Location = New System.Drawing.Point(142, 174)
+        Me.txtLastname.Name = "txtLastname"
+        Me.txtLastname.Size = New System.Drawing.Size(230, 27)
+        Me.txtLastname.TabIndex = 7
+        '
+        'cbGender
+        '
+        Me.cbGender.FormattingEnabled = True
+        Me.cbGender.Items.AddRange(New Object() {"Male", "Female"})
+        Me.cbGender.Location = New System.Drawing.Point(142, 221)
+        Me.cbGender.Name = "cbGender"
+        Me.cbGender.Size = New System.Drawing.Size(230, 28)
+        Me.cbGender.TabIndex = 8
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(415, 83)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(177, 27)
+        Me.txtSearch.TabIndex = 9
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(415, 129)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(373, 165)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(142, 265)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(94, 29)
+        Me.btnInsert.TabIndex = 11
+        Me.btnInsert.Text = "INSERT"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(278, 265)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(94, 29)
+        Me.btnUpdate.TabIndex = 12
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(598, 82)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "SEARCH"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(698, 83)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(94, 29)
+        Me.btnDelete.TabIndex = 14
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 344)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.cbGender)
+        Me.Controls.Add(Me.txtLastname)
+        Me.Controls.Add(Me.txtFirstname)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtFirstname As TextBox
+    Friend WithEvents txtLastname As TextBox
+    Friend WithEvents cbGender As ComboBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnInsert As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents btnDelete As Button
+End Class
---
+++ VBCRUD/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Data.Common
+Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        Dim ID As Integer = txtID.Text
+        Dim firstname As String = txtFirstname.Text
+        Dim lastname As String = txtLastname.Text
+        Dim gender As String = cbGender.SelectedItem
+
+        Dim query As String = "INSERT INTO member VALUES (@id, @firstname, @lastname, @gender)"
+
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-6G00CEKC;Initial Catalog=test;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@id", ID)
+                cmd.Parameters.AddWithValue("@firstname", firstname)
+                cmd.Parameters.AddWithValue("@lastname", lastname)
+                cmd.Parameters.AddWithValue("@gender", gender)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+
+                txtID.Text = ""
+                txtFirstname.Text = ""
+                txtLastname.Text = ""
+                MessageBox.Show("successfull inserted!")
+                BindData()
+
+            End Using
+        End Using
+
+    End Sub
+
+    Public Sub BindData()
+        Dim query As String = "SELECT * FROM member"
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-6G00CEKC;Initial Catalog=test;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter()
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+                        DataGridView1.DataSource = dt
+
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim query As String = "SELECT * FROM member WHERE id = '" & txtSearch.Text & "'"
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-6G00CEKC;Initial Catalog=test;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter()
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+
+                        If dt.Rows.Count > 0 Then
+                            txtID.Text = dt.Rows(0)(0).ToString()
+                            txtFirstname.Text = dt.Rows(0)(1).ToString()
+                            txtLastname.Text = dt.Rows(0)(2).ToString()
+                            cbGender.Text = dt.Rows(0)(3).ToString()
+                        Else
+                            txtID.Text = ""
+                            txtFirstname.Text = ""
+                            txtLastname.Text = ""
+                        End If
+
+
+
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim ID As Integer = txtID.Text
+        Dim firstname As String = txtFirstname.Text
+        Dim lastname As String = txtLastname.Text
+        Dim gender As String = cbGender.SelectedItem
+
+        Dim query As String = "UPDATE member SET id = @id, firstname = @firstname, lastname = @lastname, gender = @gender WHERE id = @id"
+
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-6G00CEKC;Initial Catalog=test;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@id", ID)
+                cmd.Parameters.AddWithValue("@firstname", firstname)
+                cmd.Parameters.AddWithValue("@lastname", lastname)
+                cmd.Parameters.AddWithValue("@gender", gender)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+
+                txtID.Text = ""
+                txtFirstname.Text = ""
+                txtLastname.Text = ""
+                MessageBox.Show("successfull Updated!")
+                BindData()
+
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim ID As Integer = txtID.Text
+        Dim query As String = "DELETE FROM member WHERE id = @id"
+
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-6G00CEKC;Initial Catalog=test;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@id", ID)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+
+                txtID.Text = ""
+                txtFirstname.Text = ""
+                txtLastname.Text = ""
+                MessageBox.Show("successfull Deleted!")
+                BindData()
+
+            End Using
+        End Using
+    End Sub
+End Class
